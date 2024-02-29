02806b247e275863fbd1e0dbca83ab75d815350b
Mohd Monish <surties.it@gmail.com>
2024-03-01 03:22:20 +0530

    Deployment config

--- src/server.js
+++ src/server.js
@@ -12,9 +12,7 @@ const careerRoutes = require("./Career/career.routes");
 const newsRoutes = require("./News/news.routes");
 const contact = require("./Contactus/contactus");
 const sliderRoutes = require("./ImagesSlider/imageSlider.routes");
-app.use(
-  cors({ credentials: true, origin: "https://super-pony-f201ae.netlify.app/" })
-);
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/auth", authRoutes);
