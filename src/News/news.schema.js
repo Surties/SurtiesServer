40f26db9f27b066b6c8c7054cf34538edfa3e21e
Mohd Monish <surties.it@gmail.com>
2024-04-01 23:23:49 +0530

    twitter Link

--- src/News/news.schema.js
+++ src/News/news.schema.js
@@ -6,6 +6,7 @@ const newsSchema = new mongoose.Schema({
   time: { type: String, default: Date.now() },
   imgs: [String],
   instaLink: String,
+  twitterLink: String,
   breaking: Boolean,
   author: String,
   date: { type: String, default: new Date(Date.now()) },
